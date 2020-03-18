1a4ff697e60b7b3e990862d60b873ce06552d96e
Ulises Ramirez <ulisesraf.ramirez@gmail.com>
2020-03-18 07:05:32 -0600

    cambios requeridos a la clase restaurante

--- modulos/Restaurante.js
+++ modulos/Restaurante.js
@@ -7,18 +7,52 @@ export default class Restaurante {
      * @param {Number} telefono 
      * @param {Direccion} direccion 
      */
-    constructor(nombre, telefono, direccion) {
+    constructor({ nombre, telefono, direccion }) {
         this._nombre = nombre;
         this._telefono = telefono;
         this._direccion = direccion;
         this._productos = [];
         this._pedidos = [];
     }
-    registrarProducto = producto => this._productos.push(producto);
+    buscarProducto = producto => this._productos.findIndex(p => producto.esIgual(p));
 
-    listarProductos = _ => this._productos.forEach(producto => console.log('Producto: ',producto.getDescripcion()));
+    registrarProducto = producto => {
+        if (this.buscarProducto(producto) < 0) {
+            this._productos.push(producto);
+            return true;
+        }
+        return false
+    }
 
-    registrarPedido = pedido => this._pedidos.push(pedido);
+    listarProductos = _ => this._productos.forEach(producto => console.log('Producto: ', producto.getDescripcion()));
 
-    listarPedidos = _ => this._pedidos.forEach(pedido => console.log('Pedido: ',pedido.getResumen()));
+
+
+    buscarPedido = pedido => this._pedidos.findIndex(p => p.esIgual(pedido));
+
+    registrarPedido = pedido => {
+        if (this.buscarPedido(pedido) < 0) {
+            this._pedidos.push(pedido);
+            return true;
+        }
+        return false;
+    }
+    eliminarPedido = pedido => {
+        const index = this.buscarPedido(pedido);
+        if (!(index < 0)) {
+            this._pedidos.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+    modificarPedido = (pedidoViejo, pedidoNuevo) => {
+        const index = this.buscarPedido(pedidoViejo);
+        if (!(index < 0)) {
+            this._pedidos.splice(index, 1, pedidoNuevo);
+            return true;
+        }
+        return false;
+    }
+
+    listarPedidos = _ => this._pedidos.forEach(pedido => console.log('Pedido: ', pedido.getResumen()));
 }
